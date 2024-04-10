1f345f3446d0ae9fc9dc3b6a255dfbc39ad00555
Andrew Fiasco <affiasco@umich.edu>
2024-04-10 11:34:08 -0400

    rename .sql files to match accordingly, adjust rating to int from dec

--- db/user_list.sql
+++ db/user_list.sql
@@ -1,8 +1,7 @@
--- movies.user_rating definition
+-- movies.user_list definition
 
-CREATE TABLE `user_rating` (
+CREATE TABLE `user_list` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `movie_id` int(32) UNSIGNED NOT NULL,
-  `movie_rating` DECIMAL(3, 1) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- db/user_rating.sql
+++ db/user_rating.sql
@@ -1,7 +1,8 @@
--- movies.user_list definition
+-- movies.user_rating definition
 
 CREATE TABLE `user_rating` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `movie_id` int(32) UNSIGNED NOT NULL,
+  `movie_rating` int(1) UNSIGNED NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
