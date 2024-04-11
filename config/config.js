f2bf5042b08a5e1727f96e39957f162624a597e3
Andrew Fiasco <affiasco@umich.edu>
2024-04-10 21:31:38 -0400

    readd comments, add type: module to package.json

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-export const HOST = "localhost";
-export const USER = "root";
-export const PASSWORD = "";
-export const DB = "movies";
+export const HOST = "localhost"; // Use your test host
+export const USER = "root"; // Use your host username
+export const PASSWORD = ""; // Use your host password
+export const DB = "movies"; // DB I created to use
 export const dialect = "mysql";
 export const pool = {
   max: 5,
